a1b0e7a9b39c80ae47de0f073e69de4c3155c661
Simon <simon.espigole@eisti.fr>
2015-11-23 13:24:01 +0100

    /!\ Fork /!\

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var fs = require('fs');
+var lib_func = require('lib_func');
+
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+    lib_func.createData();
+    /*fs.writeFile("/tmp/test", "Hello", function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });*/
+});
+
+
+http.listen(3000, function(){
+    console.log('Server running on : '+this.address().port);
+});
