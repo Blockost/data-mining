f3ea69ccb902b39f047d23b8273713deab46c597
Simon <simon.espigole@eisti.fr>
2015-11-30 17:17:57 +0100

    TP n°2 : k-means algo

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var fs = require('fs');
-var lib_func = require('lib_func');
+var k_nearest_neighbors = require('k-nearest_neighbors');
+var k_means = require('k-means');
 
 
 app.use(express.static('public'));
 
+app.get('/', function (req, res){
+    res.sendFile(__dirname+'/index.html');
+});
+
 // Exécution de l'algo
-lib_func.main();
+//k_nearest_neighbors.main();
+
+k_means.main();
 
 
 
 http.listen(3000, function(){
-    //console.log('Server running on : '+this.address().port);
+    console.log('Server running on : '+this.address().port);
 });
