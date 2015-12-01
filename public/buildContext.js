c498f5a775aaf0d9a4a0fa68f74d953c60b52019
Simon <simon.espigole@eisti.fr>
2015-12-01 16:25:14 +0100

    papush for Hugeuette

--- public/buildContext.js
+++ public/buildContext.js
@@ -2,23 +2,30 @@ $(document).ready(function(){
     var canvas = $('#canvas').get(0);
     if(canvas.getContext){
         var ctx = canvas.getContext('2d');
-        buildCanvas(ctx,  [[1, 2], [4, 5], [7, 8], [1, 5], [5, 9], [6, 7], [5, 5], [8, 8]]);
+        getDataAndBuildCanvas(ctx);
     }else{
         console.log('Canva don\'t supported !');
     }
 });
 
 function buildCanvas(ctx, data){
-    ctx.fillStyle = "rgb(200,0,0)";
+
     console.log(data);
     data.forEach(function(element){
-        ctx.fillRect(element[0]*600/400, element[1]*600/400, 10, 10);
+        ctx.fillStyle = getRandomRGBColor();
+        ctx.fillRect(element[0], element[1], 10, 10);
     });
 }
 
-function getData(){
-    return $.get('/data', function(response){
-        console.log(JSON.parse(response));
-        return JSON.parse(response);
+function getDataAndBuildCanvas(ctx){
+    $.get('/data', function(response){
+        buildCanvas(ctx,  JSON.parse(response));
     });
+}
+
+function getRandomRGBColor(){
+    var red = Math.floor(Math.random()*255);
+    var green = Math.floor(Math.random()*255);
+    var blue = Math.floor(Math.random()*255);
+    return "rgb("+red+","+green+","+blue+")";
 }
