74580b9327d5cdc3954e3b887a1b96b754f9745a
Simon <simon.espigole@eisti.fr>
2015-12-07 10:12:11 +0100

    Add center of classes on canvas

--- public/buildContext.js
+++ public/buildContext.js
@@ -11,15 +11,19 @@ $(document).ready(function(){
 function buildCanvas(ctx, data){
 
     console.log(data);
-    data.forEach(function(element){
+    data[0].forEach(function(element){
         ctx.fillStyle = getRandomRGBColor();
+        ctx.fillRect(element[0], element[1], 10, 10);
+    });
+    data[1].forEach(function(element){
+       ctx.fillStyle = "rgb(0, 0, 0)";
         ctx.fillRect(element[0], element[1], 10, 10);
     });
 }
 
 function getDataAndBuildCanvas(ctx){
     $.get('/data', function(response){
-        buildCanvas(ctx,  JSON.parse(response));
+        buildCanvas(ctx, JSON.parse(response));
     });
 }
 
